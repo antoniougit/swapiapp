179ba2311bab7d414d27df7d249fc5dd3f4f3a2f
George <bl1723r@gmail.com>
2019-01-21 16:34:21 +0200

    test

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
+        fetch('https://swapi.co/api/people')
             .then(response => response.json())
             .then(users => this.setState({ robots: users }));
      }
