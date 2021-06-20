d80e51d3819362d6b245f2e74fe27238d0a93bc1
BLiTz3R <bl1723r@gmail.com>
2021-06-21 02:58:36 +0300

    format App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,153 +11,169 @@ import VehiclesList from '../components/vehiclesList';
 import Navigation from '../components/Navigation';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      results: [],
-      films: [],
-      people: [],
-      species: [],
-      planets: [],
-      starships: [],      
-      vehicles: [],
-      searchfield: '',
-      route: 'selection',
+    constructor() {
+        super();
+        this.state = {
+            results: [],
+            films: [],
+            people: [],
+            species: [],
+            planets: [],
+            starships: [],
+            vehicles: [],
+            searchfield: '',
+            route: 'selection'
+        };
+    }
+
+    // TODO - Fetch all pages
+    // componentDidMount(){
+    //   const peopleURLS=[
+    //   "https://swapi.dev/api/people/1",
+    //   "https://swapi.dev/api/people/2",
+    //   "https://swapi.dev/api/people/3",
+    //   "https://swapi.dev/api/people/4",
+    //   "https://swapi.dev/api/people/5",
+    //   "https://swapi.dev/api/people/6",
+    //   "https://swapi.dev/api/people/7",
+    //   "https://swapi.dev/api/people/8",
+    //   ];
+
+    //   Promise.all(peopleURLS.map(async url => {
+    //     const resp = await fetch(url);
+    //     const mainResp = await resp.json();
+    //     let lastResp = this.state.people;
+    //     lastResp.push(mainResp.results);
+    //     var sortedArrayOfPeople = lastResp
+    //       .flat()
+    //     .sort((a, b) => a.name.localeCompare(b.name));
+    //     this.setState({people:sortedArrayOfPeople})
+    //   }))
+    // }
+
+    onButtonSelection = (route) => {
+        this.setState({ route: route });
     };
-  }
 
-  // TODO - Fetch all pages
-  // componentDidMount(){
-  //   const peopleURLS=[
-  //   "https://swapi.dev/api/people/1",
-  //   "https://swapi.dev/api/people/2",
-  //   "https://swapi.dev/api/people/3",
-  //   "https://swapi.dev/api/people/4",
-  //   "https://swapi.dev/api/people/5",
-  //   "https://swapi.dev/api/people/6",
-  //   "https://swapi.dev/api/people/7",
-  //   "https://swapi.dev/api/people/8",
-  //   ];
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    };
 
-  //   Promise.all(peopleURLS.map(async url => {
-  //     const resp = await fetch(url);
-  //     const mainResp = await resp.json();
-  //     let lastResp = this.state.people;
-  //     lastResp.push(mainResp.results);
-  //     var sortedArrayOfPeople = lastResp
-  //       .flat()
-  //     .sort((a, b) => a.name.localeCompare(b.name));
-  //     this.setState({people:sortedArrayOfPeople})
-  //   }))
-  // }
-
-  onButtonSelection = (route) => {
-    this.setState({route: route});
-  }
-
-
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value});
-  }
-
-  render() {
-    const {results,searchfield,route} = this.state;
-    if(this.state.route !== 'selection'){
-      fetch(`https://swapi.dev/api/${this.state.route}/`)
-    .then(response => response.json())
-    .then(data => this.setState({results:data.results}));
+    render() {
+        const { results, searchfield, route } = this.state;
+        if (this.state.route !== 'selection') {
+            fetch(`https://swapi.dev/api/${this.state.route}/`)
+                .then((response) => response.json())
+                .then((data) => this.setState({ results: data.results }));
+        }
+        if (route === 'films') {
+            var resultsFiltered = results
+                .filter((result) => {
+                    return result.title
+                        .toLowerCase()
+                        .includes(searchfield.toLowerCase());
+                })
+                .sort((a, b) => (a.episode_id < b.episode_id ? -1 : 1));
+        } else {
+            var resultsFiltered = results
+                .filter((result) => {
+                    return result.name
+                        .toLowerCase()
+                        .includes(searchfield.toLowerCase());
+                })
+                .sort((a, b) => (a.name < b.name ? -1 : 1));
+        }
+        if (results.length === 0 && route !== 'selection') {
+            return <h1 className="tc pt6">Loading...</h1>;
+        } else {
+            switch (route) {
+                case 'selection':
+                    return (
+                        <div className="tc">
+                            <h1 className="f1">Star Wars</h1>
+                            <h2 className="f3 pt3">
+                                Search through your favourite Star Wars data!
+                            </h2>
+                            <Selection
+                                onButtonSelection={this.onButtonSelection}
+                            />
+                        </div>
+                    );
+                case 'films':
+                    return (
+                        <div className="tc">
+                            <Navigation
+                                onButtonSelection={this.onButtonSelection}
+                            />
+                            <h1 className="f1">Star Wars</h1>
+                            <h2>Search for your favourite film!</h2>
+                            <SearchBox searchChange={this.onSearchChange} />
+                            <FilmsList films={resultsFiltered} />
+                        </div>
+                    );
+                case 'people':
+                    return (
+                        <div className="tc">
+                            <Navigation
+                                onButtonSelection={this.onButtonSelection}
+                            />
+                            <h1 className="f1">Star Wars</h1>
+                            <h2>Search for your favourite people!</h2>
+                            <SearchBox searchChange={this.onSearchChange} />
+                            <PeopleList people={resultsFiltered} />
+                        </div>
+                    );
+                case 'species':
+                    return (
+                        <div className="tc">
+                            <Navigation
+                                onButtonSelection={this.onButtonSelection}
+                            />
+                            <h1 className="f1">Star Wars</h1>
+                            <h2>Search for your favourite species!</h2>
+                            <SearchBox searchChange={this.onSearchChange} />
+                            <SpeciesList species={resultsFiltered} />
+                        </div>
+                    );
+                case 'planets':
+                    return (
+                        <div className="tc">
+                            <Navigation
+                                onButtonSelection={this.onButtonSelection}
+                            />
+                            <h1 className="f1">Star Wars</h1>
+                            <h2>Search for your favourite planet!</h2>
+                            <SearchBox searchChange={this.onSearchChange} />
+                            <PlanetsList planets={resultsFiltered} />
+                        </div>
+                    );
+                case 'starships':
+                    return (
+                        <div className="tc">
+                            <Navigation
+                                onButtonSelection={this.onButtonSelection}
+                            />
+                            <h1 className="f1">Star Wars</h1>
+                            <h2>Search for your favourite starship!</h2>
+                            <SearchBox searchChange={this.onSearchChange} />
+                            <StarshipsList starships={resultsFiltered} />
+                        </div>
+                    );
+                case 'vehicles':
+                    return (
+                        <div className="tc">
+                            <Navigation
+                                onButtonSelection={this.onButtonSelection}
+                            />
+                            <h1 className="f1">Star Wars</h1>
+                            <h2>Search for your favourite vehicle!</h2>
+                            <SearchBox searchChange={this.onSearchChange} />
+                            <VehiclesList vehicles={resultsFiltered} />
+                        </div>
+                    );
+            }
+        }
     }
-    if(route === 'films'){
-      var resultsFiltered = results
-                            .filter(result => {
-                              return result.title.toLowerCase().includes(searchfield.toLowerCase());
-                            })
-                            .sort((a, b) => a.episode_id < b.episode_id ? -1 : 1);
-    } else {
-      var resultsFiltered = results
-                            .filter(result => {
-                              return result.name.toLowerCase().includes(searchfield.toLowerCase());
-                            })
-                            .sort((a, b) => a.name < b.name ? -1 : 1);
-    }
-    if(results.length === 0 && route !== 'selection'){
-      return <h1 className='tc pt6'>Loading...</h1>
-    } else {
-    switch (route) {
-      case 'selection':
-        return(
-          <div className="tc">
-            <h1 className='f1'>Star Wars</h1>
-            <h2 className='f3 pt3'>Search through your favourite Star Wars data!</h2>
-            <Selection onButtonSelection={this.onButtonSelection}/>
-          </div>
-        );
-      case 'films':
-        return(
-          <div className="tc">
-            <Navigation onButtonSelection={this.onButtonSelection}/>  
-            <h1 className='f1'>Star Wars</h1>
-            <h2>Search for your favourite film!</h2>
-              <SearchBox searchChange={this.onSearchChange}/>
-              <FilmsList films={resultsFiltered}/>
-          </div>
-        );
-      case 'people':
-        return(
-          <div className="tc">
-            <Navigation onButtonSelection={this.onButtonSelection}/>
-            <h1 className='f1'>Star Wars</h1>
-            <h2>Search for your favourite people!</h2>
-              <SearchBox searchChange={this.onSearchChange}/>
-              <PeopleList people={resultsFiltered}/>
-          </div>
-        );
-      case 'species':
-        return(
-          <div className="tc">
-            <Navigation onButtonSelection={this.onButtonSelection}/>
-            <h1 className='f1'>Star Wars</h1>
-            <h2>Search for your favourite species!</h2>
-              <SearchBox searchChange={this.onSearchChange}/>
-              <SpeciesList species={resultsFiltered}/>
-          </div>
-        );
-      case 'planets':
-        return(
-          <div className="tc">
-            <Navigation onButtonSelection={this.onButtonSelection}/>
-            <h1 className='f1'>Star Wars</h1>
-            <h2>Search for your favourite planet!</h2>
-              <SearchBox searchChange={this.onSearchChange}/>
-              <PlanetsList planets={resultsFiltered}/>
-          </div>
-        );
-      case 'starships':
-        return(
-          <div className="tc">
-            <Navigation onButtonSelection={this.onButtonSelection}/>
-            <h1 className='f1'>Star Wars</h1>
-            <h2>Search for your favourite starship!</h2>
-              <SearchBox searchChange={this.onSearchChange}/>
-              <StarshipsList starships={resultsFiltered}/>
-          </div>
-        );
-      case 'vehicles':
-        return(
-          <div className="tc">
-            <Navigation onButtonSelection={this.onButtonSelection}/>
-            <h1 className='f1'>Star Wars</h1>
-            <h2>Search for your favourite vehicle!</h2>
-              <SearchBox searchChange={this.onSearchChange}/>
-              <VehiclesList vehicles={resultsFiltered}/>
-          </div>
-        );
-    }
-  }
-    
-
-    
-  }
 }
 
 export default App;
